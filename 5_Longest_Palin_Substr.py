3d796fb40237fde56212e47a0d829cdf34007e00
devinsuy <DevinSuy@gmail.com>
2020-10-26 14:31:34 -0700

    LeetCode #5 Longest Palindromic Substring

--- 5_Longest_Palin_Substr.py
+++ 5_Longest_Palin_Substr.py
@@ -1,22 +1,34 @@
 class Solution:
+    def longestPalindromeUtil(self, s, left, right):
+        # Continue expanding outward on both ends while in bounds and still palindrome
+        while left > -1 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        # Loop terminates after one left decrement and right increment
+        # must offset length by 1 to get actual length
+        palin_length = right - left - 1
+        if palin_length > self.max_len:
+            self.max_len = palin_length
+            self.longest = s[left+1:right]
+
     def longestPalindrome(self, s):
-        self.maxlen = 0
-        self.start = 0
+        if len(s) == 0: return ""
+        if len(s) == 1: return s
+        self.max_len = 1
+        self.longest = s[0]
 
+        # Move left and right from each "center"
         for i in range(len(s)):
-            self.expandFromCenter(s,i,i)
-            self.expandFromCenter(s,i,i+1)
-            
-        self.end = self.start + self.maxlen
-        return s[self.start:self.end]
+            # Expand outward from single letter (odd length palindromes)
+            self.longestPalindromeUtil(s, i, i)
 
+            # Expand outward from 2 letter center (even length palindromes)
+            self.longestPalindromeUtil(s, i, i+1)
+        
+        return self.longest
 
-    def expandFromCenter(self,s,l,r):
-        while l > -1 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-            
-        substr_len = r - l - 1
-        if self.maxlen < substr_len:
-            self.maxlen = substr_len
-            self.start = l + 1
+# Expected: "aba"
+s = "babad"
+sol = Solution()
+print(sol.longestPalindrome(s))
