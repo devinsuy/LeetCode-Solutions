8dc421c13537301b0570bb5ca6924d42085f83cb
devinsuy <DevinSuy@gmail.com>
2020-10-26 14:53:06 -0700

    LeetCode #91 Number of possible decodings

--- 91_Decode_Ways.py
+++ 91_Decode_Ways.py
@@ -1,37 +1,45 @@
 class Solution:
-    def isValid(self, substr):
-        if len(substr) == 0 or substr[0] == "0":
-            return False
-        val = int(substr)
+    def is_valid(self, s):
+        if len(s) == 0 or s[0] == "0": return False 
+        elif len(s) == 1: return True
+
+        val = int(s)
         return val > 0 and val < 27
-    
+
     def numDecodingsUtil(self, s, left, right):
-        # Base case, end depth traversal
-        if right >= len(s): 
+        # Base case, passed end of string, not valid path
+        if right >= len(s):
             return 0
-        
-        # Avoid redudant checks        
-        if (left, right) in self.cache:
-            return self.cache[(left, right)]
-        
-        if not self.isValid(s[left:right+1]):
+
+        # Avoid reprocessing
+        index_tuple = (left, right)
+        if index_tuple in self.cache:
+            return self.cache[index_tuple]
+
+        if not self.is_valid(s[left:right+1]):
             return 0
         else:
-            # End of string reached by decision tree
+            # End of string reached, valid decoding
             if right == len(s) - 1:
-                return 1        
-  
-        # Recurse next decision, sum single number chosen and 2 numbers chosen pathways
-        num_decodings = self.numDecodingsUtil(s, right+1, right+1) + self.numDecodingsUtil(s, right+1, right+2)
-        self.cache[(left, right)] = num_decodings
+                return 1
         
-        return num_decodings
-        
-    
-    def numDecodings(self, s: str) -> int:        
-        # Map index tuple (start, end) indices -> to its # possible decodings
+        # Calculate decodings for single digit and two digits
+        # for next decision pathway
+        a = self.numDecodingsUtil(s, right+1, right+1)
+        b = self.numDecodingsUtil(s, right+1, right+2)
+        decodings = a + b
+        self.cache[index_tuple] = decodings
+
+        return decodings
+
+    def numDecodings(self, s: str) -> int:
+        # Maps index tuples (start, end) to its number of decodings
         self.cache = {}
         a = self.numDecodingsUtil(s, 0, 0)
         b = self.numDecodingsUtil(s, 0, 1)
-        
+
         return a+b
+
+# Expected: 1
+s = Solution()
+print(s.numDecodings("2101"))
