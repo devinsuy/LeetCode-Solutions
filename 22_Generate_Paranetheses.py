3979fcad801a25ea717252f0d375290d2799f5d6
devinsuy <DevinSuy@gmail.com>
2020-12-26 16:21:24 -0800

    Modified to use append, call, pop method over deepcopy

--- 22_Generate_Paranetheses.py
+++ 22_Generate_Paranetheses.py
@@ -4,20 +4,22 @@ from copy import deepcopy
 class Solution:
     def generateParenthesis_util(self, curr_str, open_left, remain_left, remain_right):
         # Base case, end of tree reached
-        if len(curr_str) == n * 2:
+        if len(curr_str) == self.n * 2:
             self.output.append(curr_str)
 
         # Otherwise recurse:
         # Successor state will append "("
         if remain_left > 0:
-            successor_str = deepcopy(curr_str) + "("
-            self.generateParenthesis_util(successor_str, open_left+1, remain_left-1, remain_right)
+            curr_str += "("
+            self.generateParenthesis_util(curr_str, open_left+1, remain_left-1, remain_right)
+            curr_str = curr_str[:-1]
         
         # Successor state will append ")", closing a 
         # previously open "("
         if open_left > 0 and remain_right > 0:
-            successor_str = deepcopy(curr_str) + ")"
-            self.generateParenthesis_util(successor_str, open_left-1, remain_left, remain_right-1)            
+            curr_str += ")"
+            self.generateParenthesis_util(curr_str, open_left-1, remain_left, remain_right-1)
+            curr_str = curr_str[:-1]            
         
         
     def generateParenthesis(self, n: int) -> List[str]:
