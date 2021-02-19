965b6cd4c9af573bfea04341a964ca7fd7580a5f
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-02-18 16:09:03 -0800

    LeetCode #608 Tree Node Query

---
+++ 608_Tree_Node.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT t.Id, 
+    CASE
+        WHEN t.p_id IS NULL THEN "Root"
+        WHEN t.id NOT IN (SELECT p_id FROM tree WHERE p_id IS NOT NULL)  THEN "Leaf"
+        ELSE "Inner"
+    END 
+    AS "Type"
+
+FROM tree t
+ORDER BY t.Id ASC; 
