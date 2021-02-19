97b4f8b2601fae0eda0434301a7d0a9a2e343190
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-02-18 16:12:44 -0800

    LeetCode #608 Tree Node Query

--- 608_Tree_Node.sql
+++ 608_Tree_Node.sql
@@ -3,7 +3,7 @@
 SELECT t.Id, 
     CASE
         WHEN t.p_id IS NULL THEN "Root"
-        WHEN t.id NOT IN (SELECT p_id FROM tree WHERE p_id IS NOT NULL)  THEN "Leaf"
+        WHEN t.id NOT IN (SELECT DISTINCT p_id FROM tree WHERE p_id IS NOT NULL)  THEN "Leaf"
         ELSE "Inner"
     END 
     AS "Type"
