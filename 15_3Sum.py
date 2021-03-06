e2a94940339bdaa9091da0111fa30a6c28e27d37
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-05 22:25:28 -0800

    Update 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -4,25 +4,57 @@ class Solution:
         if(len(nums) < 3):
             return []
         
-        # Set of sorted triplet tuples
-        output = set([])
-        
-        # Fix the first value, the other two should sum to -nums[i] for all 3 to sum to 0
-        for i in range(len(nums)-2):
-            target = -nums[i]
-            seen = set([nums[i+1]])
-            
-            # 2 sum excluding the first value
-            for j in range(i+2, len(nums)):
-                curr = nums[j]
-                complement = target - curr
-                if(complement in seen):
-                    triplet = [-target, curr, complement]
-                    triplet.sort()
-                    output.add(tuple(triplet))                  # Sorted tuple will hash the same as any duplicates
+        output = []
+        nums.sort()   
 
-                seen.add(curr)
+        prevVal = None
+        for i in range(len(nums)):
+            # Do no reprocess duplicates (same triplets will be reached)
+            if(nums[i] == prevVal):
+                continue
+            
+            # All value at i and all values to the right of i 
+            # are positive since sorted, 0 sum is impossible
+            if(nums[i] > 0):
+                break
+                        
+            # Track duplicates remaining array to avoid duplicate triplets
+            prevLeft = prevRight = None
+            
+            target = -nums[i]
+            left = i+1
+            right = len(nums) - 1
+            
+            # 2 sum with remaining array, avoiding duplicate repairings
+            while(left < right):
+                # Do not consider adjacent repeating values (array is sorted)
+                if(nums[left] == prevLeft): 
+                    left += 1
+                    continue
+                if(nums[right] == prevRight):
+                    right -= 1
+                    continue
+                currSum = nums[left] + nums[right]
+                
+                # Triplet found, increment/decrement both pointers
+                if(currSum == target):
+                    output.append([-target, nums[left], nums[right]])
+                    prevLeft = nums[left]
+                    prevRight = nums[right]
+                    left += 1
+                    right -= 1
+
+                # Sum is too large, decrement right pointer to decrease sum next pass (since sorted)
+                elif(currSum > target):
+                    prevRight = nums[right]
+                    right -= 1    
+                
+                # Sum is too small, increment left pointer to increase sum next pass
+                else:
+                    prevLeft = nums[left]
+                    left += 1
+                    
+            prevVal = nums[i]
         
         return output
                            
-                
